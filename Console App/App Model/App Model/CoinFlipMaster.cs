fdd115e880d312d54515d8e9202526434744089d
DominikSubocz <dominiksubocz@gmail.com>
2023-11-03 11:15:02 +0000

    Implementing basic coin flip logic.
    Just started developing my app in UWP, starting off with simple coin flip logic. App will generate random result 0 or 1 (heads or tails).

---
+++ Console App/App Model/App Model/CoinFlipMaster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Model
+{
+    internal class CoinFlipMaster
+    {
+        Random rnd = new Random();
+        
+        
+        public bool flipCoin(int speed, int duration)
+        {
+            bool isHeads = false;
+            bool isTails = false;
+            int result = 0;
+            for (int i = 0; i < duration; i++) 
+            {
+                result = rnd.Next(2);
+                Console.WriteLine(result);
+            }
+        }
+
+    }
+}
