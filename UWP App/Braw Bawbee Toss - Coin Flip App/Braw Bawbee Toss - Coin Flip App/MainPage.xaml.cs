3de8a0aa72904854111fcea0efdbde08457e3720
DominikSubocz <dominiksubocz@gmail.com>
2023-11-05 13:35:28 +0000

    Added Duration of the Flip
    This will basically play a video, and then make a delay based on the duration slider's value, once time runs out the video will stop, and the result will appear.

--- UWP App/Braw Bawbee Toss - Coin Flip App/Braw Bawbee Toss - Coin Flip App/MainPage.xaml.cs
+++ UWP App/Braw Bawbee Toss - Coin Flip App/Braw Bawbee Toss - Coin Flip App/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI;
@@ -29,6 +30,8 @@ namespace Braw_Bawbee_Toss___Coin_Flip_App
 
         private int headScore = 0;
         private int tailScore = 0;
+
+        private double delayDuration = 0;
 
         public MainPage()
         {
@@ -79,7 +82,7 @@ namespace Braw_Bawbee_Toss___Coin_Flip_App
 
         private void DurChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
-
+            delayDuration = durationSlider.Value;
         }
 
         private void SpdChanged(object sender, RangeBaseValueChangedEventArgs e)
@@ -87,7 +90,7 @@ namespace Braw_Bawbee_Toss___Coin_Flip_App
 
         }
 
-        private void FlipCoin(object sender, RoutedEventArgs e)
+        private async void FlipCoin(object sender, RoutedEventArgs e)
         {
 
             bool isHeads = (new Random().Next(2) == 0);
@@ -102,10 +105,19 @@ namespace Braw_Bawbee_Toss___Coin_Flip_App
                 tailScore++;
             }
 
+            FlipBtn.IsEnabled = false;
+            FlipBtn.Background = new SolidColorBrush(Windows.UI.Colors.DarkGray);
+
+            await Task.Delay(TimeSpan.FromSeconds(delayDuration));
+
+            FlipBtn.Background = new SolidColorBrush(Windows.UI.Colors.White);
+            FlipBtn.IsEnabled = true;
             HeadsScoreTextBlock.Text = "Heads: " + headScore;
             TailsScoreTextBlock.Text = "Tails: " + tailScore;
 
 
         }
+
+ 
     }
 }
