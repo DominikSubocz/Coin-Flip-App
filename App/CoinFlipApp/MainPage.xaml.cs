4ef47f928983d5ee3eda92ac2bf2b632cc643185
DominikSubocz <dominiksubocz@gmail.com>
2023-11-15 14:14:55 +0000

    Comments & Tweaks
    Adding comments and tweaking code

--- App/CoinFlipApp/MainPage.xaml.cs
+++ App/CoinFlipApp/MainPage.xaml.cs
@@ -65,42 +65,36 @@ namespace CoinFlipApp
 
 
 
-        // Event handler 1
-        // This opens up menu pane, when the burger menu icon is clicked.
+        // Event handler 1: Open/close menu pane when the burger menu icon is clicked.
         private void MenuClicked(object sender, RoutedEventArgs e)
         {
             if (MenuSplitView.IsPaneOpen == false)
             {
 
-                MenuSplitView.IsPaneOpen = true;
+                MenuSplitView.IsPaneOpen = true;    // Opens SplitView menu pane
 
             }
 
             else if (MenuSplitView.IsPaneOpen == true)
             {
-                MenuSplitView.IsPaneOpen = false;
+                MenuSplitView.IsPaneOpen = false;  // Closes SplitView menu pane
             }
         }
 
-
-        // Event handler 2
-        // This gets run everytime the user slides the volume slider.
-        // The volume of soundPlayer will depend on the volume slider's value.
+        // Event handler 2: Adjust volume of soundPlayer based on the volume slider's value.
         private void VolumeChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
             Slider slider = sender as Slider;
-            double volumeLevel = slider.Value / 10;
-            soundPlayer.Volume = volumeLevel;
+            double volumeLevel = slider.Value / 10; // Holds volume value.
+            soundPlayer.Volume = volumeLevel;   // Changing volume level.
         }
 
-
-        // Function with correct parameter passing
-        // This function takes two parameters coinType, and duration.
-        // Then it concatenates the parameters with some trings to formulate the video file name.
-        // Which will help to choose the right video.
+        // Function: Generate video file name based on coinType and duration.
+        // Just basic concatenation of parameters which will form a full video name.
+        // It just basically picks the right video.
         private string GetVideoFileName(string coinType, int duration)
         {
-            string baseVideoName = $"{coinType}-{duration}";
+            string baseVideoName = $"{coinType}-{duration}";    
 
             string resultPlaceholder = "{result}";
 
@@ -108,58 +102,45 @@ namespace CoinFlipApp
 
             return fullVideoName;
         }
-        // Event handler 3
-        // This gets run when the user clicks the Flip History button in the pane menu.
-        // It just takes them to History Page.
-
-        private void HistoryClicked(object sender, RoutedEventArgs e)
-        {
-            if (MenuSplitView.IsPaneOpen == true)
-            {
-
-               // Frame.Navigate(typeof(History), this);
-
-            }
-        }
-
-        // Event handler 4
-        // This gets run when the user clicks the Coin Flip button.
 
 
-
+        // Event handler 4: Perform coin flip when the Coin Flip button is clicked.
         private async void FlipCoin(object sender, RoutedEventArgs e)
         {
 
 
             // Stuff for multipage databinding.
-            string mode = "Coin Flip";
-            int coinIndex = CoinComboBox.SelectedIndex;
-            string coinType = "Gold";
+            string mode = "Coin Flip";  // Current gamemode.
+            int coinIndex = CoinComboBox.SelectedIndex; // Coin type (gold, silver, bronze)
+            string coinType = "Gold";   // By default Gold.
+
+            // Switch block, it basically switches coin type based on index value.
             switch (coinIndex)
             {
                 case 0:
-                    coinType = "Gold";
+                    coinType = "Gold";  // Change to Gold.
                     break;
                 case 1:
-                    coinType = "Silver";
+                    coinType = "Silver";    // Change to Silver.
                     break;
                 case 2:
-                    coinType = "Bronze";
+                    coinType = "Bronze";    // Change to Bronze.
                     break;
 
             }
 
-            int duration = (int)durationSlider.Value;
+            int duration = (int)durationSlider.Value;   // Hold value of how long the coin will flip for. Kind of a delay.
 
-            string video = GetVideoFileName(coinType, duration);
+            string video = GetVideoFileName(coinType, duration);    // Get the right video based on picked settings.
 
-            bool isHeads = (new Random().Next(2) == 0);
-            string result = isHeads ? "Heads" : "Tails";
+            bool isHeads = (new Random().Next(2) == 0);     // Random number between 0 & 1. (False or True).
+            string result = isHeads ? "Heads" : "Tails";    // Short way of writing if else. 0 = Heads, 1 = Tails.
 
-            video = video.Replace("{result}", result);
-            soundPlayer.Play();
-            videoPlayer.Source = new Uri($"ms-appx:///Assets/Videos/{video}");
+            video = video.Replace("{result}", result);      // Video replacement.
+            soundPlayer.Play();                             // Play SFX.
+            videoPlayer.Source = new Uri($"ms-appx:///Assets/Videos/{video}");  // Change video source.
 
+            // Stuff for databinding (not working).
             var historyItem = new HistoryItem
             {
                 CoinType = coinType,
@@ -168,45 +149,41 @@ namespace CoinFlipApp
                 Result = result
             };
 
-            // Add it to the history list
+            // Add it to the history list (not working).
             coinFlipHistory.Insert(0, historyItem);
 
 
-
+            // Basic if statement, score of one will increment based on the boolean generated.
             if (isHeads)
             {
-
-                headScore++;
+                headScore++;    // Heads score incremented.
                 videoPlayer.Play();
 
             }
 
             else
             {
-                tailScore++;
+                tailScore++;    // Heads score incremented.
                 videoPlayer.Play();
-
-
-
             }
 
 
-            FlipBtn.IsEnabled = false;
-            FlipBtn.Background = new SolidColorBrush(Windows.UI.Colors.DarkGray);
+            FlipBtn.IsEnabled = false; // Disable button.
+            FlipBtn.Background = new SolidColorBrush(Windows.UI.Colors.DarkGray);   // Change colour to dark gray to show user the button is disabled.
 
-            await Task.Delay(TimeSpan.FromSeconds(duration));
+            await Task.Delay(TimeSpan.FromSeconds(duration));   // Delay based on the flip duration value.
 
 
-            FlipBtn.Background = new SolidColorBrush(Windows.UI.Colors.White);
-            FlipBtn.IsEnabled = true;
-            HeadsScoreTextBlock.Text = headScore.ToString();
-            TailsScoreTextBlock.Text = tailScore.ToString();
+            FlipBtn.Background = new SolidColorBrush(Windows.UI.Colors.White); // Change colour back to normal to show user button is enabled now.
+            FlipBtn.IsEnabled = true;                           // Enable button.
+            HeadsScoreTextBlock.Text = headScore.ToString(); // Update textboxes, convert int to string.
+            TailsScoreTextBlock.Text = tailScore.ToString();    // Update textboxes, convert int to string.
 
         }
 
         private void GuessClicked(object sender, RoutedEventArgs e)
         {
-           Frame.Navigate(typeof(GuessFlip));
+           Frame.Navigate(typeof(GuessFlip));   // Navigate to GuessFlip Page.
 
         }
     }
