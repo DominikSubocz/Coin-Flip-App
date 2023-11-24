9a6e9fb5f3ac383fe334517a1bfe349dd359c70b
DominikSubocz <dominiksubocz@gmail.com>
2023-11-24 13:34:58 +0000

    Unit Tests
    Started to implement unit tests for my app, it'll be a wee bit different
    because my app generates random outcome which means it's not always
    going to pass.

---
+++ App/CoinFlipUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoinFlipUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoinFlipUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2acb9276-8722-4f64-885c-02947cd50a33")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
